397735626317b0cf6c15a26c2a1a41569f96a407
Zone-f <1191066968@qq.com>
2022-04-05 14:02:05 +0800

    effect runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -5,7 +5,7 @@ class ReactiveEffect {
     }
     run() {
         activeEffect = this
-        this._fn();
+        return this._fn();
     }
 }
 
@@ -43,6 +43,8 @@ export function trigger(target,key){
 let activeEffect;
 export function effect(fn) {
     const _effect = new ReactiveEffect(fn);
-
-    _effect.run();
+    _effect.run()
+    
+    const runner = _effect.run.bind(_effect);
+    return runner;
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -2,18 +2,32 @@ import { effect } from "../effect";
 import { reactive } from "../reactive";
 
 describe("effect", () => {
-  it("happy path", () => {
-    const user = reactive({ age: 10 });
+    it("happy path", () => {
+        const user = reactive({ age: 10 });
 
-    let newAge;
-    effect(()=>{
-        newAge = user.age + 1
-    })
+        let newAge;
+        effect(() => {
+            newAge = user.age + 1;
+        });
 
-    expect(newAge).toBe(11)
+        expect(newAge).toBe(11);
 
-    //update
-    newAge++;
-    expect(newAge).toBe(12)
-  });
+        //update
+        newAge++;
+        expect(newAge).toBe(12);
+    });
+
+    //??runner是干嘛的??
+    it("返回当前依赖", () => {
+        let foo = 10;
+        const runner = effect(() => {
+            foo++;
+            return "foo";
+        });
+
+        expect(foo).toBe(11)
+        const r = runner();
+        expect(foo).toBe(12);
+        expect(r).toBe("foo");
+    });
 });
