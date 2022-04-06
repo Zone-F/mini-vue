2eb5882c73b97abf4acd424d3578240da9386b00
Zone-f <1191066968@qq.com>
2022-04-06 20:50:48 +0800

    实现effect的scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,16 +1,17 @@
 class ReactiveEffect {
     private _fn: any;
-    constructor(fn) {
+    public scheduler: Function | undefined;
+    constructor(fn,scheduler) {
         this._fn = fn;
+        this.scheduler = scheduler
     }
     run() {
-        activeEffect = this
+        activeEffect = this;
         return this._fn();
     }
 }
 
 const targetMap = new Map();
-
 // 收集依赖
 export function track(target, key) {
     let depsMap = targetMap.get(target);
@@ -19,32 +20,44 @@ export function track(target, key) {
         targetMap.set(target, depsMap);
     }
 
-    let dep = depsMap.get(key)
-    if(!dep){
-        dep = new Set()
-        depsMap.set(key,dep)
+    let dep = depsMap.get(key);
+    if (!dep) {
+        dep = new Set();
+        depsMap.set(key, dep);
     }
 
-    dep.add(activeEffect)
+    dep.add(activeEffect);
 }
 
 // 触发依赖
-export function trigger(target,key){
-    let depsMap = targetMap.get(target)
-    let dep = depsMap.get(key)
-    dep.map((effect)=>effect.run())
-    // depsMap.map((fn)=>{
-    //     fn().run
-    // })
-}
+export function trigger(target, key) {
+    let depsMap = targetMap.get(target);
+    let dep = depsMap.get(key);
 
+    for (const effect of dep) {
+        if (effect.scheduler) {
+          effect.scheduler();
+        } else {
+          effect.run();
+        }
+      }
+
+    // dep?.map((effect) => {
+    //     if(effect.scheduler){
+    //         effect.scheduler();
+    //     }else{
+    //         effect.run()
+
+    //     }
+    // });
+}
 
 // export function track(target,key){};
 let activeEffect;
-export function effect(fn) {
-    const _effect = new ReactiveEffect(fn);
-    _effect.run()
-    
+export function effect(fn, options = {}) {
+    const _effect = new ReactiveEffect(fn,options?.scheduler);
+    _effect.run();
+
     const runner = _effect.run.bind(_effect);
     return runner;
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -25,9 +25,44 @@ describe("effect", () => {
             return "foo";
         });
 
-        expect(foo).toBe(11)
+        expect(foo).toBe(11);
         const r = runner();
         expect(foo).toBe(12);
         expect(r).toBe("foo");
+    });
+
+    it("scheduler", () => {
+        //1、通过effect的第二个参数,给定一个 scheduler(function)
+        //2、effect 第一次执行的 还会执行 fn
+        //3、当响应式对象 set update 是不执行fn而是执行scheduler
+        //4、如果执行 runner 则会再次执行 fn
+
+        let dummy;
+        let run;
+        const scheduler = jest.fn(() => {
+            run = runner;
+        });
+
+        const obj = reactive({ foo: 1 });
+
+        const runner = effect(
+            () => {
+                dummy = obj.foo;
+            },
+            { scheduler }
+        );
+
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+
+        // 触发set
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        // runner fn 不会再次触发
+        expect(dummy).toBe(1);
+        
+        // run 时会再次执行fn
+        run()
+        expect(dummy).toBe(2);
     });
 });
