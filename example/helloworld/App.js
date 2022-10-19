660ca49ff5890ce24c4d4421a549e0fc7f4eb25e
zone98f <1191066968@qq.com>
2022-10-19 21:07:55 +0800

    feat:实现初始化element主流程

---
+++ example/helloworld/App.js
@@ -0,0 +1,23 @@
+import { h } from "../../lib/guide-mini-vue.esm.js";
+export const App = {
+    // 必须要写 render
+    render(){
+        return h(
+            "div",
+            {
+              id: "root",
+              class: ["red", "hard"],
+            },
+            // "hi, " + this.msg
+            // string
+            // "hi, mini-vue"
+            // Array
+            [h("p", { class:"red"}, "hi"), h("p", {class:"blue"}, "mini-vue")]
+          );
+    },
+    setup(){
+        return {
+            msg:"mini-vue"
+        }
+    }
+}
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,14 +1,48 @@
 import { createComponentInstance, setupComponent } from "./component";
 export function render(vnode, container) {
   //patch
-  //
   patch(vnode, container);
 }
 
 function patch(vnode, container) {
-  processComponent(vnode, container);
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
 }
 
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type);
+
+  const { children } = vnode;
+
+  // children
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+
+  // props
+  const { props } = vnode;
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  container.append(el);
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
+}
 function processComponent(vnode: any, container: any) {
   mountComponent(vnode, container);
 }
@@ -25,3 +59,4 @@ function setupRenderEffect(instance: any, container) {
 
   patch(subTree, container);
 }
+
